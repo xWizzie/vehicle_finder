bfcf9feec045679dafc6093c5e914e0aa8dda2ab
Lazaros Georgiadis <paoki200@gmail.com>
2023-05-23 21:48:38 +0300

    Changes changes changes...

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,9 +14,9 @@ const Navbar = ({ handleRouteClick }) => {
                     <li className="nav-item">
                         <Link className="nav-link" to="/about" onClick={() => handleRouteClick('About')} >About</Link>
                     </li>
-                    <li className="nav-item">
+                    {/* <li className="nav-item">
                         <Link className="nav-link" to="/cars" onClick={() => handleRouteClick('Cars')}>Cars</Link>
-                    </li>
+                    </li> */}
                     <li className="nav-item">
                         <Link className="nav-link" to="/contact" onClick={() => handleRouteClick('Contact')}>Contact</Link>
                     </li>
